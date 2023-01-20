ca3b902d769c684a2d85a1963a95fe3688ba9c78
21MH5A0368 <112760852+21MH5A0368@users.noreply.github.com>
2023-01-20 11:34:44 +0530

    //#include<bits/stdc++.h> #include<iostream> #include<iomanip> using namespace std; int main() {     int r;     float a;     cin>>r;     a=3.14*r*r;     //printf("%.2f",a);     cout<<fixed<<setprecision(2)<<a; }

---
+++ Area_of_a_circle.cpp
@@ -0,0 +1,13 @@
+//#include<bits/stdc++.h>
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    int r;
+    float a;
+    cin>>r;
+    a=3.14*r*r;
+    //printf("%.2f",a);
+    cout<<fixed<<setprecision(2)<<a;
+}
